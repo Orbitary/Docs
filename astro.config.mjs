7dddefb09d8da3a2caf1d2111f1f17092d94e124
ImBit <151650938+ImBit@users.noreply.github.com>
2026-04-03 20:19:11 +0100

    :art: Small refinements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,103 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
+import starlightUtils from '@lorenzo_lewis/starlight-utils'
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Bit Docs',
+			editLink: {
+				baseUrl: 'https://github.com/ImBit/Bit-Docs/',
+			},
+			plugins: [
+				starlightUtils({
+					multiSidebar: {
+						switcherStyle: 'horizontalList',
+					},
+				}),
+			],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'Bits',
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ slug: 'bits' },
+						{
+							label: 'Getting started',
+							autogenerate: { directory: 'bits/getting-started' },
+						},
+						// {
+						// 	label: 'API',
+						// 	items: [
+						// 		{
+						// 			label: 'Commands',
+						// 			collapsed: false,
+						// 			autogenerate: { directory: 'bits/api/commands' },
+						// 		},
+						// 		{
+						// 			label: 'Text & formatting',
+						// 			collapsed: true,
+						// 			autogenerate: { directory: 'bits/api/text' },
+						// 		},
+						// 		{
+						// 			label: 'Utilities',
+						// 			collapsed: true,
+						// 			autogenerate: { directory: 'bits/api/utilities' },
+						// 		},
+						// 	],
+						// },
+						// {
+						// 	label: 'Paper',
+						// 	badge: { text: 'Paper', variant: 'note' },
+						// 	autogenerate: { directory: 'bits/paper' },
+						// },
+						// {
+						// 	label: 'Velocity',
+						// 	badge: { text: 'Velocity', variant: 'tip' },
+						// 	autogenerate: { directory: 'bits/velocity' },
+						// },
+						// {
+						// 	label: 'Contributing',
+						// 	collapsed: true,
+						// 	autogenerate: { directory: 'bits/contributing' },
+						// },
+						// {
+						// 	label: 'Misc',
+						// 	collapsed: true,
+						// 	autogenerate: { directory: 'bits/misc' },
+						// },
 					],
 				},
+
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'PacketNinja',
+					items: [
+						{ slug: 'packetninja' },
+						// {
+						// 	label: 'Getting started',
+						// 	autogenerate: { directory: 'packetninja/getting-started' },
+						// },
+						// {
+						// 	label: 'Usage',
+						// 	autogenerate: { directory: 'packetninja/usage' },
+						// },
+						// {
+						// 	label: 'API',
+						// 	badge: { text: 'Coming soon', variant: 'caution' },
+						// 	autogenerate: { directory: 'packetninja/api' },
+						// },
+						// {
+						// 	label: 'Contributing',
+						// 	collapsed: true,
+						// 	autogenerate: { directory: 'packetninja/contributing' },
+						// },
+						// {
+						// 	label: 'Misc',
+						// 	collapsed: true,
+						// 	autogenerate: { directory: 'packetninja/misc' },
+						// },
+					],
 				},
 			],
 		}),
 	],
-});
+})
---
+++ src/content/docs/bits/getting-started/what-is-bits.md
@@ -0,0 +1,10 @@
+---
+title: What is Bits?
+description: How to add Bits to your Gradle or Maven project.
+sidebar:
+  order: 2
+  # badge: { text: 'Paper', variant: 'note' }
+  draft: true
+---
+
+Test text!
